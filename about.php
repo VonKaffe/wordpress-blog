3bf8f9405c5cca7b6c433644de126874c627fa4d
Simon Appeltofft <31758722+VonKaffe@users.noreply.github.com>
2018-09-09 17:41:29 +0200

    Update about.php

--- about.php
+++ about.php
@@ -3,11 +3,7 @@
     <img src="<?php echo get_template_directory_uri() . "/img/default-avatar.png" ?>" class="profile-img"/>
     <article class="blog-article-aboutme">
       <h4>About me</h4>
-      <p>Cat ipsum dolor sit amet, wake up human for food at 4am
-        instead of drinking water from the cat bowl, make sure to steal water
-        from the toilet it's 3am, time to create some chaos yet meow all night
-        really likes hummus. I like cats because they are fat and fluffy this
-        cat happen now, it was too purr-fect!!!</p>
+      <p>oof, I like tacos its very nice stuff.</p>
     </article>
     <article class="blog-article-categories">
       <h4>Categories</h4>
